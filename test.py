b218a007c5853a6a25d9381301386e8969ef99e7
Your Name <you@example.com>
2019-04-09 11:57:11 -0300

    file of test

--- test.py
+++ test.py
@@ -1,14 +1,24 @@
-from random import randint
-primos = set()
+lista = ["marcos","mateus","marlus"]
+lista_saida = []
+tam_entrada = len(lista)
 
+primeiro_nome = lista[0]
+'''menor = len(primeiro_nome)
 
-while len(primos) < 5:
-  cont=0
-  num = (randint(2,100))
-  for a in range (1,num+1):
-    if num%a == 0:
-      cont=cont+1
-  if cont == 2:
-    primos.add(num)
-    
-print ("Conjunto a =", primos)
+for h in range (0,tam_entrada):
+	if menor > len(lista[h]):
+		primeiro_nome = lista[h]
+		menor = len(lista[h])'''
+
+tam_first_name = len (primeiro_nome)
+
+for i in range (0,tam_first_name):
+	cont = 0
+	vogal = primeiro_nome[i]
+	for j in range (0,tam_entrada):
+		if vogal in lista[j]:
+			cont = cont +1
+	if cont == tam_entrada:
+		lista_saida.append(vogal)
+		
+print (lista_saida)
